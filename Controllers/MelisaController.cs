653786cd04cfa2c01ba1bc0c2e2d312c64006f12
MELISA-MARIA CRIȘAN <melisa.crisan@stud.ubbcluj.ro>
2025-11-24 18:55:41 +0200

    Rezolvare tema

---
+++ Controllers/MelisaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Crisan_Melisa_Lab1_Master.Controllers
+{
+    public class MelisaController : Controller
+    {
+        
+        public string Index()
+        {
+            return "Bine ai venit pe pagina!";
+        }
+
+        public string Despre()
+        {
+            return "Aceasta este o pagina dezvoltata la modulul Medii de programare avansate.";
+        }
+
+        public string Detalii(string nume, int varsta)
+        {
+            return $"Salut {nume}! Ai {varsta} ani. Mesaj generat din controller-ul Melisa.";
+        }
+
+    }
+}
